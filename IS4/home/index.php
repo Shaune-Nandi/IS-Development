091e2b45fb1ccb6b02392c97dff3b0d31d8cfc15
Shaune <42217566+Shaune-Nandi@users.noreply.github.com>
2019-11-03 19:45:48 +0300

    Add files via upload

--- IS4/home/index.php
+++ IS4/home/index.php
@@ -11,8 +11,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet" />
   <link href="./../css/styles.css" type="text/css" rel="stylesheet" />
@@ -22,7 +21,7 @@ session_start();
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="./../home/index.php">
       <img src="./../images/logo/logo.png" class="img-fluid" width="40px"><strong>StrathCafe</strong>
     </a>
@@ -37,43 +36,20 @@ session_start();
         <li class="nav-item">
           <a class="nav-link" href="./../cafeterias/cafeterias.php">Cafeterias</a>
         </li>
-        <a href="#"><button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#myModal">
-            ORDERS = <span id="finalOrder">0</span>
-          </button></a>
-      </ul>
 
-      <?php
-      if (isset($_SESSION['loggedin'])) {
-        echo '        
-          <ul class="nav navbar-nav">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="fa fa-user"><strong> ' . $_SESSION['FirstName'] . " " . $_SESSION['LastName'] . ' - ' . $_SESSION['AdmissionNumber'] . '</strong></span>
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="./../authentication/profile.php">View Profile</a>
-                <a class="dropdown-item" href="./../authentication/logout.php">Logout</a>
-              </div>
-            </li>
-          </ul>
-          ';
-      } else {
-        echo '
-          <ul class="navbar-nav">
-          <li class="nav-item">
-              <a href="./../authentication/login.html">
-                <button class="btn btn-outline-light  my-2 my-sm-0" type="submit" id="log">Login</button>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="./../registration/registration.html">
-                <button class="btn btn-outline-danger  my-2 my-sm-0" type="submit" id="log">Register</button>
-              </a>
-            </li>
-          </ul>
-          ';
-      }
-      ?>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="./../authentication/login.php">
+            <button class="btn btn-outline-light  my-2 my-sm-0" type="submit" id="log">Login</button>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="./../registration/register.php">
+            <button class="btn btn-outline-danger  my-2 my-sm-0" type="submit" id="log">Register</button>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 
@@ -86,7 +62,7 @@ session_start();
           <h1>StrathCafe</h1>
           <h2><strong>Are you hungry?</strong></h2>
           <p>
-            Look no further, StrathCafe 
+            Look no further, StrathCafe
             by the click of a button.
           </p>
           <center>
@@ -189,7 +165,7 @@ session_start();
         <div class="col-md-6">
           <a href="https://twitter.com/strathu/"><i class="fa fa-twitter" style="font-size: 30px;padding: 12px"></i></a>
           <a href="https://www.instagram.com/strathmore.university/"><i class="fa fa-instagram" style="font-size: 30px;padding: 12px"></i>
-          <a href="https://www.facebook.com/StrathmoreUniversity/"><i class="fa fa-facebook" style="font-size: 30px;padding: 12px"></i></a>
+            <a href="https://www.facebook.com/StrathmoreUniversity/"><i class="fa fa-facebook" style="font-size: 30px;padding: 12px"></i></a>
         </div>
       </div>
     </div>
@@ -198,15 +174,9 @@ session_start();
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-    crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
   <script src="./js/scripts.js"></script>
 </body>
